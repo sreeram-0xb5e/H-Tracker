6ab7a34e96d55642fa94f4c95708e65c32120797
sreeram-0xb5e <sreeram2910@gmail.com>
2017-04-11 11:16:10 +0530

    Fixed the multiple entries problem

--- Extension/background.js
+++ Extension/background.js
@@ -1,8 +1,16 @@
+var x = 0;
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
+
     chrome.tabs.getSelected(null,function(tab) {
+        
         document.getElementById("url").value  = tab.url;
         document.getElementById("tabid").value = tab.id;
         document.getElementById("time").value = Date();
+        if (x < 1)
+        {
+        document.getElementById('submit').click();
+        x = x+1;
+    	}
     });
 
     //document.getElementById("submit").click();
